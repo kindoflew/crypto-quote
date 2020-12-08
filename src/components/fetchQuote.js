9a4df0a9a8b161611218f74324c20a614e6ffd40
Lewis <davanzo.lewis@gmail.com>
2020-12-08 14:13:24 -0500

    refactors return value

--- src/components/fetchQuote.js
+++ src/components/fetchQuote.js
@@ -1,9 +1,5 @@
 export async function fetchQuote() {
   let response = await fetch('https://api.quotable.io/random?minLength=50&maxLength=80');
   let data = await response.json();
-  return joinQuoteAndAuthor(data.content, data.author);
-}
-
-function joinQuoteAndAuthor(quote, author) {
-  return `${quote.toUpperCase()} —${author.toUpperCase()}`;
+  return `${data.content.toUpperCase()} — ${data.author.toUpperCase()}`;
 }
