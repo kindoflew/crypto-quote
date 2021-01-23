462f84eeebb8e5740abddaf0eddf38add7661855
Lewis <davanzo.lewis@gmail.com>
2021-01-22 20:15:39 -0500

    changes modal color scheme

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -11,9 +11,11 @@
 <svelte:window on:keyup|preventDefault={(e) => e.key === 'Escape' && dispatch('closeModal')}/>
  
 <section on:click|self={() => dispatch('closeModal')}>
-  <div>
+  <div class="modal">
+    <div class="wrapper">
     <p class="message">You did it!</p>
     <p>{quote}</p>
+    </div>
     <Button content="Close" clickFunction={() => dispatch('closeModal')} />
   </div>
 </section>
@@ -31,28 +33,37 @@
     align-items: center;
   }
 
-  div {
+  .modal {
     background-color: var(--main-bg);
     color: var(--font-color);
     border-radius: 10px;
-    padding: 2rem;
+    padding: 1.5rem 1rem;
     width: 40vw;
     text-align: center;
     box-shadow: 5px 5px 5px rgba(61, 28, 1, 0.612);
   }
 
+  .wrapper {
+    background-color: var(--game-bg);
+    border-radius: 10px;
+    box-shadow: 2px 2px 2px rgba(61, 28, 1, 0.612);
+    width: 94%;
+    padding: 0.5rem;
+    margin: 0 auto 1.5rem;
+  }
+
   .message {
     font-size: 3rem;
     margin-top: 0;
+    margin-bottom: 1rem;
   }
 
   p {
     font-size: 1.5rem;
-    margin-bottom: 3rem;
   }
 
   @media(max-width: 1000px) {
-    div {
+    .modal {
       width: 70vw;
     }
   }
