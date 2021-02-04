cc862ceb3991c07f22b8901b9f6c467de00968cd
Lewis <davanzo.lewis@gmail.com>
2021-02-04 11:20:33 -0500

    refactors quote and solved into stores

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,3 +1,11 @@
-import { writable } from "svelte/store";
+import { writable, derived } from "svelte/store";
 
 export const answer = writable([]);
+
+export const quote = writable("");
+
+export const solved = derived([answer, quote], ([$answer, $quote]) => isSolved($answer, $quote));
+
+function isSolved(a, q) {
+  return a.join("") === q;
+}
