c7b72a2d9d4f48d99d9842969b0b8b0340e0d3d7
Lewis <davanzo.lewis@gmail.com>
2021-02-04 12:17:43 -0500

    reverts changes to store

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,11 +1,3 @@
-import { writable, derived } from "svelte/store";
+import { writable } from "svelte/store";
 
 export const answer = writable([]);
-
-export const quote = writable("");
-
-export const solved = derived([answer, quote], ([$answer, $quote]) => isSolved($answer, $quote));
-
-function isSolved(a, q) {
-  return a.join("") === q;
-}
