4958fb5483d62056de24fc1fa16e1546b69c8a1b
kindoflew <davanzo.lewis@gmail.com>
2025-07-27 14:29:15 -0400

    update quote api

--- src/utils/fetchQuote.js
+++ src/utils/fetchQuote.js
@@ -1,5 +1,6 @@
 export async function fetchQuote() {
-  let response = await fetch('https://api.quotable.io/random?minLength=50&maxLength=80&tags=famous-quotes');
-  let data = await response.json();
-  return `${data.content.toUpperCase()} — ${data.author.toUpperCase()}`;
+  const response = await fetch('https://quoteslate.vercel.app/api/quotes/random?minLength=50&maxLength=80');
+  const data = await response.json();
+  console.log('data: ', data)
+  return `${data.quote.toUpperCase()} — ${data.author.toUpperCase()}`;
 }
